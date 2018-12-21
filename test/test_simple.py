d5100f977e50689054b955446533d52219e2deed
gzagatti <gzagatti@gmail.com>
2018-12-21 17:44:21 -0200

    Split simple test in two modules.

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Test features of the DAG created by the plugin."""
+""" Test the plugin in the context of a simple test directory structure. """
 
 import pytest
 from _pytest._code.code import ExceptionChainRepr
@@ -25,52 +25,6 @@ def simple_testdir(testdir):
                 assert 0
             """,
     )
-
-
-def test_vanilla_execution(testdir, simple_testdir):
-    """ Test that vanilla execution does not use the plugin. """
-    result = testdir.runpytest()
-    result.assert_outcomes(failed=2, passed=2)
-
-
-def test_returned_values(testdir, simple_testdir):
-    """ Test pytest-airflow returns values as expected. """
-    result = testdir.runpytest("--airflow")
-    dag, source, sink = result.ret
-
-    result.assert_outcomes(passed=4)
-
-    assert isinstance(dag, DAG)
-    assert isinstance(source, BaseOperator)
-    assert isinstance(sink, BaseOperator)
-
-    assert dag.dag_id == "pytest"
-    assert source.task_id == "__pytest_source"
-    assert sink.task_id == "__pytest_sink"
-
-    tasks = dag.topological_sort()
-
-    assert tasks[0] == source
-    assert tasks[-1] == sink
-
-
-def test_custom_dag_name(testdir, simple_testdir):
-    """ Test custom DAG name."""
-    result = testdir.runpytest("--airflow", "--dag-id", "foo")
-    dag, _, _ = result.ret
-
-    assert dag.dag_id == "foo"
-
-
-def test_custom_terminating_tasks_name(testdir, simple_testdir):
-    """ Test custom source and sink name."""
-    result = testdir.runpytest("--airflow", "--source", "foo")
-    _, source, _ = result.ret
-    assert source.task_id == "foo"
-
-    result = testdir.runpytest("--airflow", "--sink", "foo")
-    _, _, sink = result.ret
-    assert sink.task_id == "foo"
 
 
 def test_returned_tasks(testdir, simple_testdir):
