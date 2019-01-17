c3d48dc93bc86c156fbfa8d4fc0b8b6923bba6ea
gzagatti <gzagatti@gmail.com>
2019-01-17 14:07:52 -0500

    Adds unit test.

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+from collections import defaultdict
+
 pytest_plugins = ["pytester"]
+
+
+class MockClass:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
 
 class MockTaskInstance:
@@ -14,8 +21,19 @@ class MockTaskInstance:
     def xcom_push(self, key, val):
         self.xcom[key] = val
 
+    def xcom_pull(self, task_id, key):
+        return self.xcom.get(task_id, {}).get(key, None)
+
 
 @pytest.fixture()
 def mock_context():
     """ Creates a mock context for running deferred tests and fixtures. """
-    return {"ti": MockTaskInstance()}
+    ctx = defaultdict(MockClass)
+    ctx["ti"] = MockTaskInstance()
+    return ctx
+
+
+@pytest.fixture()
+def mock_object():
+    """ Creates an instance a mock object which allows for arbitrary attributes. """
+    return lambda **kwargs: MockClass(**kwargs)
